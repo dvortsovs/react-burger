1617ec5b90b554eb09f493db75df52855b2501a8
Sevastian Dvortsov <86145076+dvortsovs@users.noreply.github.com>
2022-03-20 16:39:12 +0300

    feat: add AppHeader component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import './app.module.css';
+import appStyle from './app.module.css';
+import AppHeader from "../app-header/app-header";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className={appStyle.app}>
+        <AppHeader />
     </div>
+
+
   );
 }
 
