487a2cb1bd9e5a3a57ab0dc968675ef28a2b0584
Sevastian Dvortsov <86145076+dvortsovs@users.noreply.github.com>
2022-03-28 17:08:16 +0300

    feat: add api response

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
-import data from "../../utils/data";
 import appStyle from './app.module.css';
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
+import {api} from "../../constants/api";
 
 function App() {
+
+    const [state, setState] = React.useState({isLoading: false, isError: false, data: []})
+
+    React.useEffect(() => {
+        setState({...state, isLoading: true});
+        fetch(api)
+            .then(res => res.json())
+            .then(res => setState({...state, data: res.data, isLoading: false}))
+            .catch((err) => {
+                setState({...state, isLoading: false, isError: true})
+                console.log(err)
+            })
+    }, [])
 
   return (
     <div className={appStyle.app}>
         <AppHeader />
         <main className={`${appStyle.main}`}>
-            <BurgerIngredients data={data} />
-            <BurgerConstructor data={data} />
+            {!state.isLoading && !state.isError && <BurgerIngredients data={state.data}/> }
+            {!state.isLoading && !state.isError && !!state.data.length && <BurgerConstructor data={state.data} /> }
         </main>
     </div>
-
-
   );
 }
 
