9585617212bf8a4d3d5c4e9d099baea954d5e8c8
Sevastian Dvortsov <86145076+dvortsovs@users.noreply.github.com>
2022-04-25 09:47:09 +0300

    feat: add burger-ingredients nav logic

--- src/components/ingredient-list/ingredient-list.jsx
+++ src/components/ingredient-list/ingredient-list.jsx
@@ -5,10 +5,10 @@ import IngredientCard from "../ingredient-card/ingredient-card";
 import {useSelector} from "react-redux";
 
 
-function IngredientList({type}) {
+const IngredientList = React.forwardRef(({type}, ref) => {
     const ingredients = useSelector(state => state.ingredientsList.ingredients)
     return (
-        <article>
+        <article ref={ref}>
             <h2 className={`mt-10 mb-6 text text_type_main-medium`}>{type.title}</h2>
             <ul className={`${ingredientListStyle.list} ml-4 mr-4`}>
                 {ingredients.map((ingredient) => {
@@ -20,7 +20,7 @@ function IngredientList({type}) {
             </ul>
         </article>
     )
-}
+})
 
 IngredientList.propTypes = {
     type: PropTypes.objectOf(PropTypes.string),
