954a01f43b26e60f428dc264acc4b6b54a568243
Sevastian Dvortsov <86145076+dvortsovs@users.noreply.github.com>
2022-05-30 12:17:26 +0300

    feat: refactor pages with Form

--- src/pages/register-page/register-page.jsx
+++ src/pages/register-page/register-page.jsx
@@ -1,56 +1,49 @@
 import React, {useState} from 'react';
-import {Link} from "react-router-dom";
 import {Button, Input} from "@ya.praktikum/react-developer-burger-ui-components";
 import registerPageStyles from './register-page.module.css'
+import Form from "../../components/form/form";
 
 export default function RegisterPage() {
-    const [value, setValue] = useState('')
+    const [emailValue, setEmailValue] = useState('')
+    const [nameValue, setNameValue] = useState('')
+    const [passwordValue, setPasswordValue] = useState('')
+    const [passwordHideState, setPasswordHideState] = useState('password')
     // const inputRef = useRef(null)
+
     return (
-        <div className={`${registerPageStyles.main}`}>
-            <div className={`${registerPageStyles.container}`}>
-                <h1 className={`text text_type_main-medium mb-6`}>
-                    Регистрация
-                </h1>
-                <form className={`${registerPageStyles.form}`}>
-                    <Input
-                        type='text'
-                        placeholder='Имя'
-                        value={value}
-                        onChange={(e) => setValue(e.target.value)}
-                        size='default'
-                    />
-                    <Input
-                        type='email'
-                        placeholder='E-mail'
-                        value={value}
-                        onChange={(e) => setValue(e.target.value)}
-                        errorText='error'
-                        error={false}
-                        size='default'
-                    />
-                    <Input
-                        type='password'
-                        placeholder='Пароль'
-                        value={value}
-                        onChange={(e) => setValue(e.target.value)}
-                        size='default'
-                        icon='ShowIcon'
-                    />
-                    <Button type='primary' size='medium'>
-                        Зарегистрироваться
-                    </Button>
-                </form>
-                <p className={`text text_type_main-default text_color_inactive mt-20`}>
-                    Уже зарегистрированы?
-                    <Link
-                        to='/login'
-                        className={`${registerPageStyles.link} text text_type_main-default ml-2`}
-                    >
-                        Войти
-                    </Link>
-                </p>
-            </div>
-        </div>
+        <section className={`${registerPageStyles.main}`}>
+            <Form title='Регистрация' links={[
+                {title: 'Уже зарегистрированы?', link: '/login', linkTitle: 'Войти'},
+            ]}>
+                <Input
+                    type='text'
+                    placeholder='Имя'
+                    value={nameValue}
+                    onChange={(e) => setNameValue(e.target.value)}
+                    size='default'
+                />
+                <Input
+                    type='email'
+                    placeholder='E-mail'
+                    value={emailValue}
+                    onChange={(e) => setEmailValue(e.target.value)}
+                    errorText='error'
+                    error={false}
+                    size='default'
+                />
+                <Input
+                    type={passwordHideState}
+                    placeholder='Пароль'
+                    icon={"ShowIcon"}
+                    onIconClick={() => passwordHideState === 'password' ? setPasswordHideState('text') : setPasswordHideState('password')}
+                    value={passwordValue}
+                    onChange={(e) => setPasswordValue(e.target.value)}
+                    errorText='error'
+                    error={false}
+                    size='default'
+                />
+                <Button>Войти</Button>
+            </Form>
+        </section>
     )
 }
--- src/pages/reset-password/reset-password-page.jsx
+++ src/pages/reset-password/reset-password-page.jsx
@@ -1,51 +1,41 @@
 import React, {useState} from 'react';
-import {Link} from "react-router-dom";
 import {Button, Input} from "@ya.praktikum/react-developer-burger-ui-components";
 import resetPasswordPageStyles from './reset-password-page.module.css'
+import Form from "../../components/form/form";
 
 export default function ResetPasswordPage() {
-    const [value, setValue] = useState('')
+    const [codeValue, setCodeValue] = useState('')
+    const [passwordValue, setPasswordValue] = useState('')
+    const [passwordHideState, setPasswordHideState] = useState('password')
     // const inputRef = useRef(null)
+
     return (
-        <div className={`${resetPasswordPageStyles.main}`}>
-            <div className={`${resetPasswordPageStyles.container}`}>
-                <h1 className={`text text_type_main-medium mb-6`}>
-                    Восстановление пароля
-                </h1>
-                <form className={`${resetPasswordPageStyles.form}`}>
-                    <Input
-                        type='password'
-                        placeholder='Введите новый пароль'
-                        value={value}
-                        onChange={(e) => setValue(e.target.value)}
-                        errorText='error'
-                        error={false}
-                        size='default'
-                        icon={"ShowIcon"}
-                    />
-                    <Input
-                        type='text'
-                        placeholder='Введите код из письма'
-                        value={value}
-                        onChange={(e) => setValue(e.target.value)}
-                        errorText='error'
-                        error={false}
-                        size='default'
-                    />
-                    <Button type='primary' size='medium'>
-                        Сохранить
-                    </Button>
-                </form>
-                <p className={`text text_type_main-default text_color_inactive mt-20`}>
-                    Вспомнили пароль?
-                    <Link
-                        to='/login'
-                        className={`${resetPasswordPageStyles.link} text text_type_main-default ml-2`}
-                    >
-                        Войти
-                    </Link>
-                </p>
-            </div>
-        </div>
+        <section className={`${resetPasswordPageStyles.main}`}>
+            <Form title='Восстановление пароля' links={[
+                {title: 'Вспомнили пароль?', link: '/login', linkTitle: 'Войти'},
+            ]}>
+                <Input
+                    type={passwordHideState}
+                    placeholder='Введите новый пароль'
+                    icon={"ShowIcon"}
+                    onIconClick={() => passwordHideState === 'password' ? setPasswordHideState('text') : setPasswordHideState('password')}
+                    value={passwordValue}
+                    onChange={(e) => setPasswordValue(e.target.value)}
+                    errorText='error'
+                    error={false}
+                    size='default'
+                />
+                <Input
+                    type='text'
+                    placeholder='Введите код из письма'
+                    value={codeValue}
+                    onChange={(e) => setCodeValue(e.target.value)}
+                    errorText='error'
+                    error={false}
+                    size='default'
+                />
+                <Button>Сохранить</Button>
+            </Form>
+        </section>
     )
 }
