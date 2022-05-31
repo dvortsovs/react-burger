ed918b36ac3e065280f911edbbf5446c6fc12c72
Sevastian Dvortsov <86145076+dvortsovs@users.noreply.github.com>
2022-06-01 00:15:47 +0300

    feat: add validateForm, refactor register-page

--- src/pages/register-page/register-page.jsx
+++ src/pages/register-page/register-page.jsx
@@ -2,47 +2,59 @@ import React, {useState} from 'react';
 import {Button, Input} from "@ya.praktikum/react-developer-burger-ui-components";
 import registerPageStyles from './register-page.module.css'
 import Form from "../../components/form/form";
+import {validateForm} from "../../services/utils";
 
 export default function RegisterPage() {
-    const [emailValue, setEmailValue] = useState('')
-    const [nameValue, setNameValue] = useState('')
-    const [passwordValue, setPasswordValue] = useState('')
-    const [passwordHideState, setPasswordHideState] = useState('password')
+    const [emailValue, setEmailValue] = useState('');
+    const [nameValue, setNameValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
+    const [passwordHideState, setPasswordHideState] = useState('password');
+    const [nameError, setNameError] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [passError, setPassError] = useState(false);
     // const inputRef = useRef(null)
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
 
     return (
         <section className={`${registerPageStyles.main}`}>
-            <Form title='Регистрация' links={[
+            <Form onSubmit={onSubmit} title='Регистрация' links={[
                 {title: 'Уже зарегистрированы?', link: '/login', linkTitle: 'Войти'},
             ]}>
                 <Input
                     type='text'
                     placeholder='Имя'
                     value={nameValue}
-                    onChange={(e) => setNameValue(e.target.value)}
+                    onChange={(e) => validateForm(e, 'name', setNameValue, setNameError)}
                     size='default'
+                    error={nameError}
+                    errorText='Поле не должно быть пустым'
                 />
                 <Input
                     type='email'
                     placeholder='E-mail'
                     value={emailValue}
-                    onChange={(e) => setEmailValue(e.target.value)}
-                    errorText='error'
-                    error={false}
+                    onChange={(e) => validateForm(e, 'email', setEmailValue, setEmailError)}
+                    errorText='Некорректный e-mail'
+                    error={emailError}
                     size='default'
                 />
                 <Input
                     type={passwordHideState}
                     placeholder='Пароль'
                     icon={"ShowIcon"}
-                    onIconClick={() => passwordHideState === 'password' ? setPasswordHideState('text') : setPasswordHideState('password')}
+                    onIconClick={() => passwordHideState === 'password'
+                        ? setPasswordHideState('text')
+                        : setPasswordHideState('password')}
                     value={passwordValue}
-                    onChange={(e) => setPasswordValue(e.target.value)}
-                    errorText='error'
-                    error={false}
+                    onChange={(e) => validateForm(e, 'pass', setPasswordValue, setPassError)}
+                    errorText='Поле не должно быть пустым'
+                    error={passError}
                     size='default'
                 />
-                <Button>Войти</Button>
+                <Button disabled={nameError || passError || emailError}>Зарегистрироваться</Button>
             </Form>
         </section>
     )
---
+++ src/services/utils.js
@@ -0,0 +1,21 @@
+const validateForm = (e, type, setValue, setError) => {
+    switch (type) {
+        case 'email':
+            setValue(e.target.value)
+            const reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+            console.log(reg.test(e.target.value))
+            setError(!reg.test(e.target.value))
+            break
+        case 'pass':
+            setValue(e.target.value)
+            setError(e.target.value === '')
+            break
+        case 'name':
+            setValue(e.target.value)
+            setError(e.target.value === '')
+            break
+        default: return
+    }
+}
+
+export {validateForm}
